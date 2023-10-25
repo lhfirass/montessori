bad1623523f949b0dfc10d5dad7fe076c26e3588
lhfirass <81319618+lhfirass@users.noreply.github.com>
2023-10-25 17:33:58 +0100

    Hero
    + I just finished working on header section, and make it responsive.
    + I start working on hero section and make it responsive on large screen and small.

--- src/Hero.js
+++ src/Hero.js
@@ -1,9 +1,25 @@
 const Hero = () => {
     return ( 
         <section className="hero">
-            
+            <div className="message">
+                <div>
+                    <p>ألبومات مونتيسوري بالعربي</p>
+                    <p>“يجب أن يكون الهدف من التعلم في مرحلة الطفولة المبكرة  هو تنشيط رغبة الطفل الطبيعية للتعلم و ليس بإكراهه على التعلم”</p>
+                </div>
+                <div className="actionBtn">
+                    <a href="">
+                    تحميل مختصر 
+                    </a>
+                    <a href="">
+                    للتواصل
+                    </a>
+                </div>
+            </div>
+            <div>
+                <img src="hero-pic.png" alt="" />
+            </div>
         </section>
     );
 }
- 
+
 export default Hero;
