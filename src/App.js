652acb32daea475327c1f67d46cbf7c4dfd1f726
lhfirass <81319618+lhfirass@users.noreply.github.com>
2023-10-28 14:16:05 +0100

    f-btn

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import Hero from './Hero';
 import FPIXELS from './FPIXELS';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Stars from './fpixelsWidgets/Stars';
+import MyButton from './fpixelsWidgets/MyButton';
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('المزيد عن المؤلفة');
+  const [color, setColor] = useState('white');
+  const [backgroundColor, setBackgroundColor] = useState('#222222');
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
   return (
     <Router>
       <div className="App">
@@ -17,6 +25,12 @@ function App() {
           <Header />
           <Hero />
           <About />
+          <MyButton
+            text={text}
+            color={color}
+            backgroundColor={backgroundColor}
+            onClick={handleClick}
+      />
         </Route>
         {/*end*/};
         {/*FPIXELS landing page*/};
